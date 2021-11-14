eb0d3a87bc99038358b72bc8e8559e7c9845fdd2
Egor Rubtsov <rubtsove777@gmail.com>
2021-11-14 15:20:54 +0500

    Initial Commit

---
+++ CheckScoringGame/CheckScoringGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckScoringGameApp.swift
+//  CheckScoringGame
+//
+//  Created by Егор on 14.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct CheckScoringGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
